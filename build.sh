73bad68d832286c16e99fd2d4fb7e84c68adb977
Richard Wilkes <wilkes@me.com>
2021-09-18 14:59:32 -0700

    Bump golangci-lint to v1.42.1

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ set -eo pipefail
 
 trap 'echo -e "\033[33;5mBuild failed on build.sh:$LINENO\033[0m"' ERR
 
-GOLANGCI_LINT_VERSION=1.38.0
+GOLANGCI_LINT_VERSION=1.42.1
 RACE=-race
 
 for arg in "$@"
