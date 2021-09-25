1b153fee35226e7ae1199fb07592bdc01a6b878b
Richard Wilkes <wilkes@me.com>
2021-09-25 13:18:36 -0700

    Simplify

--- images.go
+++ images.go
@@ -10,99 +10,76 @@
 package unison
 
 import (
-	_ "embed" // Needed for image embedding
+	"embed"
+	"sync"
 
 	"github.com/richardwilkes/toolbox/log/jot"
 )
 
 var (
-	//go:embed resources/images/arrows_horizontal.png
-	arrowsHorizontalData []byte
-	arrowsHorizontalImg  *Image
-
-	//go:embed resources/images/arrows_horizontal_vertical.png
-	arrowsHorizontalVerticalData []byte
-	arrowsHorizontalVerticalImg  *Image
-
-	//go:embed resources/images/arrows_left_diagonal.png
-	arrowsLeftDiagonalData []byte
-	arrowsLeftDiagonalImg  *Image
-
-	//go:embed resources/images/arrows_right_diagonal.png
-	arrowsRightDiagonalData []byte
-	arrowsRightDiagonalImg  *Image
-
-	//go:embed resources/images/arrows_vertical.png
-	arrowsVerticalData []byte
-	arrowsVerticalImg  *Image
-
-	//go:embed resources/images/closed_hand.png
-	closedHandData []byte
-	closedHandImg  *Image
-
-	//go:embed resources/images/error.png
-	errorData []byte
-	errorImg  *Image
-
-	//go:embed resources/images/open_hand.png
-	openHandData []byte
-	openHandImg  *Image
-
-	//go:embed resources/images/question.png
-	questionData []byte
-	questionImg  *Image
+	//go:embed resources/images
+	imageFS   embed.FS
+	imageLock sync.Mutex
+	imageMap  = make(map[string]*Image)
 )
 
 // ArrowsHorizontalImage returns the standard horizontal arrows, pointing left and right.
 func ArrowsHorizontalImage() *Image {
-	return retrieveImage(arrowsHorizontalData, &arrowsHorizontalImg)
+	return getImage("arrows_horizontal")
 }
 
 // ArrowsHorizontalVerticalImage returns the standard horizontal and vertical arrows, pointing left, right, up and down.
 func ArrowsHorizontalVerticalImage() *Image {
-	return retrieveImage(arrowsHorizontalVerticalData, &arrowsHorizontalVerticalImg)
+	return getImage("arrows_horizontal_vertical")
 }
 
 // ArrowsLeftDiagonalImage returns the standard left diagonal arrows, pointing northwest and southeast.
 func ArrowsLeftDiagonalImage() *Image {
-	return retrieveImage(arrowsLeftDiagonalData, &arrowsLeftDiagonalImg)
+	return getImage("arrows_left_diagonal")
 }
 
 // ArrowsRightDiagonalImage returns the standard right diagonal arrows, pointing northeast and southwest.
 func ArrowsRightDiagonalImage() *Image {
-	return retrieveImage(arrowsRightDiagonalData, &arrowsRightDiagonalImg)
+	return getImage("arrows_right_diagonal")
 }
 
 // ArrowsVerticalImage returns the standard vertical arrows, pointing up and down.
 func ArrowsVerticalImage() *Image {
-	return retrieveImage(arrowsVerticalData, &arrowsVerticalImg)
+	return getImage("arrows_vertical")
 }
 
 // ClosedHandImage returns the standard closed hand image.
 func ClosedHandImage() *Image {
-	return retrieveImage(closedHandData, &closedHandImg)
+	return getImage("closed_hand")
 }
 
 // ErrorImage returns the standard error alert image.
 func ErrorImage() *Image {
-	return retrieveImage(errorData, &errorImg)
+	return getImage("error")
 }
 
 // OpenHandImage returns the standard open hand image.
 func OpenHandImage() *Image {
-	return retrieveImage(openHandData, &openHandImg)
+	return getImage("open_hand")
 }
 
 // QuestionImage returns the standard question alert image.
 func QuestionImage() *Image {
-	return retrieveImage(questionData, &questionImg)
+	return getImage("question")
 }
 
-func retrieveImage(imgData []byte, img **Image) *Image {
-	if *img == nil {
+func getImage(name string) *Image {
+	imageLock.Lock()
+	defer imageLock.Unlock()
+	img, exists := imageMap[name]
+	if !exists {
 		var err error
-		*img, err = NewImageFromBytes(imgData, 0.5)
+		var data []byte
+		data, err = imageFS.ReadFile("resources/images/" + name + ".png")
 		jot.FatalIfErr(err)
+		img, err = NewImageFromBytes(data, 0.5)
+		jot.FatalIfErr(err)
+		imageMap[name] = img
 	}
-	return *img
+	return img
 }
