5fa57bce57982a7b1af3cad00356d3a4d62c31d5
Richard Wilkes <wilkes@me.com>
2022-01-02 16:07:38 -0800

    Update copyright notices

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-# Copyright ©2021 by Richard A. Wilkes. All rights reserved.
+# Copyright ©2021-2022 by Richard A. Wilkes. All rights reserved.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, version 2.0. If a copy of the MPL was not distributed with
