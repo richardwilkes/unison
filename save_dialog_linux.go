246034646f8f0fede57bca7a18dcbe6b739b4d32
Richard Wilkes <wilkes@me.com>
2022-09-20 17:59:12 -0700

    first step: add save with kdialog

--- save_dialog_linux.go
+++ save_dialog_linux.go
@@ -9,6 +9,94 @@
 
 package unison
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/richardwilkes/toolbox/log/jot"
+)
+
+type linuxFileDialog struct {
+	fallback SaveDialog
+	results  []string
+}
+
 func platformNewSaveDialog() SaveDialog {
-	return NewCommonSaveDialog()
+	return &linuxFileDialog{fallback: NewCommonSaveDialog()}
+}
+
+func (d *linuxFileDialog) InitialDirectory() string {
+	return d.fallback.InitialDirectory()
+}
+
+func (d *linuxFileDialog) SetInitialDirectory(dir string) {
+	d.fallback.SetInitialDirectory(dir)
+}
+
+func (d *linuxFileDialog) AllowedExtensions() []string {
+	return d.fallback.AllowedExtensions()
+}
+
+func (d *linuxFileDialog) SetAllowedExtensions(extensions ...string) {
+	d.fallback.SetAllowedExtensions(extensions...)
+}
+
+func (d *linuxFileDialog) RunModal() bool {
+	kdialog, err := exec.LookPath("kdialog")
+	if err != nil {
+		kdialog = ""
+	}
+	if os.Getenv("KDE_FULL_SESSION") != "" && kdialog != "" {
+		return d.runKDialog(kdialog)
+	}
+
+	var zenity string
+	if zenity, err = exec.LookPath("zenity"); err != nil {
+		zenity = ""
+	}
+	if zenity != "" {
+		return d.runZenity(zenity)
+	}
+	if kdialog != "" {
+		return d.runKDialog(kdialog)
+	}
+	return d.fallback.RunModal()
+}
+
+func (d *linuxFileDialog) runKDialog(kdialog string) bool {
+	ext := ""
+	allowed := d.fallback.AllowedExtensions()
+	if len(allowed) != 0 {
+		revised := make([]string, len(allowed))
+		for i, one := range allowed {
+			revised[i] = "*." + one
+		}
+		ext = revised[0]
+		allowed = revised
+	}
+	cmd := exec.Command(kdialog, "--getsavefilename", d.InitialDirectory()+"/untitled"+ext)
+	if len(allowed) != 0 {
+		list := strings.Join(allowed, " ")
+		cmd.Args = append(cmd.Args, fmt.Sprintf("%[1]s (%[1]s)", list))
+	}
+	out, err := cmd.Output()
+	if err != nil {
+		jot.Error(err)
+		return false
+	}
+	if cmd.ProcessState.ExitCode() != 0 {
+		return false
+	}
+	d.fallback.(*fileDialog).paths = strings.Split(string(out), "\n")
+	return true
+}
+
+func (d *linuxFileDialog) runZenity(zenity string) bool {
+	return true
+}
+
+func (d *linuxFileDialog) Path() string {
+	return d.fallback.Path()
 }
