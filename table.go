96b405f1ebfe211aaaff97a70c97525c0d896a2a
Richard Wilkes <wilkes@me.com>
2022-10-16 15:19:34 -0700

    Switch from option to control the for discontiguous selection modifier
    As before, the command key is also accepted. Part of the work for #19.

--- table.go
+++ table.go
@@ -740,7 +740,7 @@ func (t *Table[T]) DefaultMouseDown(where Point, button, clickCount int, mod Mod
 				t.selAnchor = id
 				t.notifyOfSelectionChange()
 			}
-		case mod&(OptionModifier|CommandModifier) != 0: // Toggle single row
+		case mod&(ControlModifier|CommandModifier) != 0: // Toggle single row
 			if t.selMap[id] {
 				delete(t.selMap, id)
 			} else {
