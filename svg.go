2a86b271ee0d6d988f4fc7abd2fe044e83104ba5
Richard Wilkes <wilkes@me.com>
2023-08-17 17:08:27 -0700

    Add more SVG loading options
    The new loaders can read a standard svg file without needing to have the
    path "d" attribute extracted manually. They can also read svg's that
    contain multiple paths. Other types of data are still ignored.

--- svg.go
+++ svg.go
@@ -10,6 +10,13 @@
 package unison
 
 import (
+	_ "embed"
+	"encoding/xml"
+	"io"
+	"strconv"
+	"strings"
+
+	"github.com/richardwilkes/toolbox/errs"
 	"github.com/richardwilkes/toolbox/log/jot"
 )
 
@@ -17,20 +24,61 @@ var _ Drawable = &DrawableSVG{}
 
 // Pre-defined SVG images used by Unison.
 var (
-	BrokenImageSVG         = MustSVG(NewSize(24, 24), "M19 13a1 1 0 0 0-1 1v.39l-1.48-1.48a2.77 2.77 0 0 0-3.93 0l-.7.7-2.48-2.49a2.86 2.86 0 0 0-3.93 0L4 12.6V7a1 1 0 0 1 1-1h8a1 1 0 0 0 0-2H5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-5a1 1 0 0 0-1-1ZM5 20a1 1 0 0 1-1-1v-3.57l2.9-2.9a.79.79 0 0 1 1.09 0l3.17 3.17 4.29 4.3Zm13-1a1 1 0 0 1-.18.53L13.31 15l.7-.7a.78.78 0 0 1 1.1 0L18 17.22Zm1-17a3 3 0 0 0-2.6 1.5 1 1 0 0 0 .37 1.37 1 1 0 0 0 1.36-.37A1 1 0 0 1 20 5a1 1 0 0 1-1 1 1 1 0 0 0 0 2 3 3 0 0 0 0-6Zm.38 7.08A1 1 0 0 0 18.8 9l-.18.06-.18.09-.15.13A1 1 0 0 0 18 10a1 1 0 0 0 .29.71 1 1 0 0 0 1.42 0A1 1 0 0 0 20 10a1 1 0 0 0-.62-.92Z")
-	CircledChevronRightSVG = MustSVG(NewSize(512, 512), "M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zm113.9 231L234.4 103.5c-9.4-9.4-24.6-9.4-33.9 0l-17 17c-9.4 9.4-9.4 24.6 0 33.9L285.1 256 183.5 357.6c-9.4 9.4-9.4 24.6 0 33.9l17 17c9.4 9.4 24.6 9.4 33.9 0L369.9 273c9.4-9.4 9.4-24.6 0-34z")
-	CircledExclamationSVG  = MustSVG(NewSize(512, 512), "M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346 7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z")
-	CircledQuestionSVG     = MustSVG(NewSize(512, 512), "M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z")
-	CheckmarkSVG           = MustSVG(NewSize(172, 172), "m149.285 31.294-11.86-8.063c-3.283-2.222-7.779-1.37-9.975 1.887l-58.143 85.741-26.72-26.72c-2.791-2.79-7.34-2.79-10.13 0L22.3 94.295c-2.79 2.79-2.79 7.339 0 10.156l41.088 41.087c2.3 2.3 5.917 4.058 9.173 4.058 3.257 0 6.538-2.042 8.657-5.117l69.979-103.236c2.222-3.256 1.37-7.727-1.913-9.95z")
-	ChevronRightSVG        = MustSVG(NewSize(320, 512), "M285.476 272.971 91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z")
-	CircledXSVG            = MustSVG(NewSize(512, 512), "M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z")
-	DashSVG                = MustSVG(NewSize(448, 512), "M416 256c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h352c17.7 0 32 14.3 32 32z")
-	DocumentSVG            = MustSVG(NewSize(384, 512), "M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm160-14.1v6.1H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z")
-	SortAscendingSVG       = MustSVG(NewSize(512, 512), "M240 96h64a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm0 128h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm256 192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-256-64h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm-64 0h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352z")
-	SortDescendingSVG      = MustSVG(NewSize(512, 512), "M304 416h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM16 160h48v304a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V160h48c14.21 0 21.38-17.24 11.31-27.31l-80-96a16 16 0 0 0-22.62 0l-80 96C-5.35 142.74 1.77 160 16 160zm416 0H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-64 128H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM496 32H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z")
-	TriangleExclamationSVG = MustSVG(NewSize(576, 512), "M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346 7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z")
-	WindowMaximizeSVG      = MustSVG(NewSize(512, 512), "M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-16 160H64v-84c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12v84z")
-	WindowRestoreSVG       = MustSVG(NewSize(512, 512), "M512 48v288c0 26.5-21.5 48-48 48h-48V176c0-44.1-35.9-80-80-80H128V48c0-26.5 21.5-48 48-48h288c26.5 0 48 21.5 48 48zM384 176v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48h288c26.5 0 48 21.5 48 48zm-68 28c0-6.6-5.4-12-12-12H76c-6.6 0-12 5.4-12 12v52h252v-52z")
+	//go:embed resources/images/broken_image.svg
+	brokenImageSVG string
+	BrokenImageSVG = MustSVGFromContentString(brokenImageSVG)
+
+	//go:embed resources/images/circled_chevron_right.svg
+	circledChevronRightSVG string
+	CircledChevronRightSVG = MustSVGFromContentString(circledChevronRightSVG)
+
+	//go:embed resources/images/circled_exclamation.svg
+	circledExclamationSVG string
+	CircledExclamationSVG = MustSVGFromContentString(circledExclamationSVG)
+
+	//go:embed resources/images/circled_question.svg
+	circledQuestionSVG string
+	CircledQuestionSVG = MustSVGFromContentString(circledQuestionSVG)
+
+	//go:embed resources/images/checkmark.svg
+	checkmarkSVG string
+	CheckmarkSVG = MustSVGFromContentString(checkmarkSVG)
+
+	//go:embed resources/images/chevron_right.svg
+	chevronRightSVG string
+	ChevronRightSVG = MustSVGFromContentString(chevronRightSVG)
+
+	//go:embed resources/images/circled_x.svg
+	circledXSVG string
+	CircledXSVG = MustSVGFromContentString(circledXSVG)
+
+	//go:embed resources/images/dash.svg
+	dashSVG string
+	DashSVG = MustSVGFromContentString(dashSVG)
+
+	//go:embed resources/images/document.svg
+	documentSVG string
+	DocumentSVG = MustSVGFromContentString(documentSVG)
+
+	//go:embed resources/images/sort_ascending.svg
+	sortAscendingSVG string
+	SortAscendingSVG = MustSVGFromContentString(sortAscendingSVG)
+
+	//go:embed resources/images/sort_descending.svg
+	sortDescendingSVG string
+	SortDescendingSVG = MustSVGFromContentString(sortDescendingSVG)
+
+	//go:embed resources/images/triangle_exclamation.svg
+	triangleExclamationSVG string
+	TriangleExclamationSVG = MustSVGFromContentString(triangleExclamationSVG)
+
+	//go:embed resources/images/window_maximize.svg
+	windowMaximizeSVG string
+	WindowMaximizeSVG = MustSVGFromContentString(windowMaximizeSVG)
+
+	//go:embed resources/images/window_restore.svg
+	windowRestoreSVG string
+	WindowRestoreSVG = MustSVGFromContentString(windowRestoreSVG)
 )
 
 // DrawableSVG makes an SVG conform to the Drawable interface.
@@ -39,22 +87,24 @@ type DrawableSVG struct {
 	Size Size
 }
 
-// SVG holds an SVG path. Note that this is a subset of SVG: just the 'd' attribute of the 'path' directive.
+// SVG holds an SVG.
 type SVG struct {
 	size          Size
 	unscaledPath  *Path
 	scaledPathMap map[Size]*Path
 }
 
-// MustSVG creates a new SVG and panics if an error would be generated. The 'size' should be gotten from the original
-// SVG's 'viewBox' parameter.
+// MustSVG creates a new SVG the given svg path string (the contents of a single "d" attribute from an SVG "path"
+// element) and panics if an error would be generated. The 'size' should be gotten from the original SVG's 'viewBox'
+// parameter.
 func MustSVG(size Size, svg string) *SVG {
 	s, err := NewSVG(size, svg)
 	jot.FatalIfErr(err)
 	return s
 }
 
-// NewSVG creates a new SVG. The 'size' should be gotten from the original SVG's 'viewBox' parameter.
+// NewSVG creates a new SVG the given svg path string (the contents of a single "d" attribute from an SVG "path"
+// element). The 'size' should be gotten from the original SVG's 'viewBox' parameter.
 func NewSVG(size Size, svg string) (*SVG, error) {
 	unscaledPath, err := NewPathFromSVGString(svg)
 	if err != nil {
@@ -65,6 +115,74 @@ func NewSVG(size Size, svg string) (*SVG, error) {
 		unscaledPath:  unscaledPath,
 		scaledPathMap: make(map[Size]*Path),
 	}, nil
+}
+
+// MustSVGFromContentString creates a new SVG and panics if an error would be generated. The content should contain
+// valid SVG file data. Note that this only reads a very small subset of an SVG currently. Specifically, the "viewBox"
+// attribute and any "d" attributes from enclosed SVG "path" elements.
+func MustSVGFromContentString(content string) *SVG {
+	s, err := NewSVGFromContentString(content)
+	jot.FatalIfErr(err)
+	return s
+}
+
+// NewSVGFromContentString creates a new SVG. The content should contain valid SVG file data. Note that this only reads
+// a very small subset of an SVG currently. Specifically, the "viewBox" attribute and any "d" attributes from enclosed
+// SVG "path" elements.
+func NewSVGFromContentString(content string) (*SVG, error) {
+	return NewSVGFromReader(strings.NewReader(content))
+}
+
+// MustSVGFromReader creates a new SVG and panics if an error would be generated. The reader should contain valid SVG
+// file data. Note that this only reads a very small subset of an SVG currently. Specifically, the "viewBox" attribute
+// and any "d" attributes from enclosed SVG "path" elements.
+func MustSVGFromReader(r io.Reader) *SVG {
+	s, err := NewSVGFromReader(r)
+	jot.FatalIfErr(err)
+	return s
+}
+
+// NewSVGFromReader creates a new SVG. The reader should contain valid SVG file data. Note that this only reads a very
+// small subset of an SVG currently. Specifically, the "viewBox" attribute and any "d" attributes from enclosed SVG
+// "path" elements.
+func NewSVGFromReader(r io.Reader) (*SVG, error) {
+	var svgXML struct {
+		ViewBox string `xml:"viewBox,attr"`
+		Paths   []struct {
+			Path string `xml:"d,attr"`
+		} `xml:"path"`
+	}
+	if err := xml.NewDecoder(r).Decode(&svgXML); err != nil {
+		return nil, errs.NewWithCause("unable to decode SVG", err)
+	}
+	svg := &SVG{scaledPathMap: make(map[Size]*Path)}
+	var width, height string
+	if parts := strings.Split(svgXML.ViewBox, " "); len(parts) == 4 {
+		width = parts[2]
+		height = parts[3]
+	}
+	v, err := strconv.ParseFloat(width, 64)
+	if err != nil || v < 1 || v > 4096 {
+		return nil, errs.NewWithCause("unable to determine SVG width", err)
+	}
+	svg.size.Width = float32(v)
+	v, err = strconv.ParseFloat(height, 64)
+	if err != nil || v < 1 || v > 4096 {
+		return nil, errs.NewWithCause("unable to determine SVG height", err)
+	}
+	svg.size.Height = float32(v)
+	for i, svgPath := range svgXML.Paths {
+		var p *Path
+		if p, err = NewPathFromSVGString(svgPath.Path); err != nil {
+			return nil, errs.NewWithCausef(err, "unable to decode SVG: path element #%d", i)
+		}
+		if svg.unscaledPath == nil {
+			svg.unscaledPath = p
+		} else {
+			svg.unscaledPath.Path(p, false)
+		}
+	}
+	return svg, nil
 }
 
 // Size returns the original size.
