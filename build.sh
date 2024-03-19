ec87c5a5aab8da5dd6442e664b86efe53e628b7c
Richard Wilkes <wilkes@me.com>
2024-03-18 21:06:02 -0700

    Remove unnecessary files and update build script

--- build.sh
+++ build.sh
@@ -37,6 +37,19 @@ go generate ./gen/enumgen.go
 echo -e "\033[33mBuilding Go code...\033[0m"
 go build -v ./...
 
+# Run the linters
+if [ "$LINT"x == "1x" ]; then
+  GOLANGCI_LINT_VERSION=$(curl --head -s https://github.com/golangci/golangci-lint/releases/latest | grep location: | sed 's/^.*v//' | tr -d '\r\n' )
+  TOOLS_DIR=$(go env GOPATH)/bin
+  if [ ! -e "$TOOLS_DIR/golangci-lint" ] || [ "$("$TOOLS_DIR/golangci-lint" version 2>&1 | awk '{ print $4 }' || true)x" != "${GOLANGCI_LINT_VERSION}x" ]; then
+    echo -e "\033[33mInstalling version $GOLANGCI_LINT_VERSION of golangci-lint into $TOOLS_DIR...\033[0m"
+    mkdir -p "$TOOLS_DIR"
+    curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$TOOLS_DIR" v$GOLANGCI_LINT_VERSION
+  fi
+  echo -e "\033[33mLinting...\033[0m"
+  "$TOOLS_DIR/golangci-lint" run
+fi
+
 # Run the tests
 if [ "$TEST"x == "1x" ]; then
   if [ -n "$RACE" ]; then
@@ -45,16 +58,4 @@ if [ "$TEST"x == "1x" ]; then
     echo -e "\033[33mTesting...\033[0m"
   fi
   go test $RACE ./...
-fi
-
-# Run the linters
-if [ "$LINT"x == "1x" ]; then
-  TOOLS_DIR=$PWD/tools
-  if [ ! -e "$TOOLS_DIR/golangci-lint" ] || [ "$("$TOOLS_DIR/golangci-lint" version 2>&1 | awk '{ print $4 }' || true)x" != "${GOLANGCI_LINT_VERSION}x" ]; then
-    echo -e "\033[33mInstalling version $GOLANGCI_LINT_VERSION of golangci-lint into $TOOLS_DIR...\033[0m"
-    mkdir -p "$TOOLS_DIR"
-    curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$TOOLS_DIR" v$GOLANGCI_LINT_VERSION
-  fi
-  echo -e "\033[33mRunning Go linters...\033[0m"
-  $TOOLS_DIR/golangci-lint run
 fi
