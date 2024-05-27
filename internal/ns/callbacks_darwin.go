f18d1fc17d2415715cdda14a7194070af2636c32
Richard Wilkes <wilkes@me.com>
2024-05-27 13:31:37 -0700

    Update copyright notices

--- internal/ns/callbacks_darwin.go
+++ internal/ns/callbacks_darwin.go
@@ -1,4 +1,4 @@
-// Copyright ©2021-2022 by Richard A. Wilkes. All rights reserved.
+// Copyright (c) 2021-2024 by Richard A. Wilkes. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with
