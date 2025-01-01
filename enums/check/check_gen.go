3537bef787bc494f0758c7fb63db426259f860ad
Richard Wilkes <wilkes@me.com>
2025-01-01 12:22:01 -0800

    Bump copyright date

--- enums/check/check_gen.go
+++ enums/check/check_gen.go
@@ -1,6 +1,6 @@
 // Code generated from "enum.go.tmpl" - DO NOT EDIT.
 
-// Copyright (c) 2021-2024 by Richard A. Wilkes. All rights reserved.
+// Copyright (c) 2021-2025 by Richard A. Wilkes. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with
