4d74218ac1d44cd24d338dcbf97cce1f4ef80451
Richard Wilkes <wilkes@me.com>
2025-11-11 10:42:39 -0800

    Remove unnecessary _GNU_SOURCE define on linux

--- internal/plaf/build.go
+++ internal/plaf/build.go
@@ -4,7 +4,7 @@ package plaf
 #cgo darwin CFLAGS: -DPLAF_DARWIN -Wno-deprecated-declarations -x objective-c
 #cgo darwin LDFLAGS: -framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL
 
-#cgo linux CFLAGS: -DPLAF_LINUX -D_GNU_SOURCE
+#cgo linux CFLAGS: -DPLAF_LINUX
 #cgo linux LDFLAGS: -lGL -lX11 -lXrandr -lXxf86vm -lXi -lXcursor -lm -lXinerama -ldl -lrt
 
 #cgo windows CFLAGS: -DPLAF_WINDOWS
