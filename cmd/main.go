d23d734fac1eb661578ce81cd078bcca6b47cd8d
Richard Wilkes <wilkes@me.com>
2026-03-26 15:43:05 -0700

    x11 testing main

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/richardwilkes/toolbox/v2/xflag"
+	"github.com/richardwilkes/toolbox/v2/xos"
+	"github.com/richardwilkes/toolbox/v2/xslog"
+	"github.com/richardwilkes/unison/internal/x11"
+)
+
+var (
+	x11Conn         *x11.Conn
+	x11ContentScale float32 = 1
+)
+
+func main() {
+	logCfg := xslog.Config{Console: true}
+	logCfg.AddFlags()
+	xflag.Parse()
+	xos.ExitIfErr(start())
+	xos.Exit(0)
+}
+
+func start() error {
+	var err error
+	if x11Conn, err = x11.NewConn(); err != nil {
+		return err
+	}
+	if x11ContentScale, err = x11GetContentScale(); err != nil {
+		return err
+	}
+	fmt.Printf("content scale: %f\n", x11ContentScale)
+	fmt.Printf("BEFORE clipboard: %q\n", x11Conn.GetClipboardText())
+	x11Conn.SetClipboardText("Yo!")
+	fmt.Printf(" AFTER clipboard: %q\n", x11Conn.GetClipboardText())
+	x11Conn.PushClipboardToManager()
+	x11Conn.Close()
+	return nil
+}
+
+func x11GetContentScale() (float32, error) {
+	format, actualPropertyType, value, err := x11Conn.GetProperty(x11Conn.RootWindow(), x11.AtomResourceManager,
+		x11.AtomString, 0, 100_000_000, false)
+	if err != nil {
+		return 1, err
+	}
+	if format == 8 && actualPropertyType == x11.AtomString {
+		for _, line := range strings.Split(string(value), "\n") {
+			const xftDPI = "Xft.dpi:"
+			if strings.HasPrefix(line, xftDPI) {
+				var dpi int
+				if dpi, err = strconv.Atoi(strings.TrimSpace(strings.TrimPrefix(line, xftDPI))); err == nil {
+					return float32(dpi) / 96, nil
+				}
+			}
+		}
+	}
+	return 1, nil
+}
