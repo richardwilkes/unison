5fe3f28fcbcc871071d363c2301b547099059963
Richard Wilkes <wilkes@me.com>
2026-06-19 10:38:37 -0700

    Use a hex-encoded random 12 bytes as the unique portion of the private data type

--- data_type.go
+++ data_type.go
@@ -10,18 +10,18 @@
 package unison
 
 import (
-	"strings"
+	"crypto/rand"
+	"encoding/hex"
 
-	"github.com/richardwilkes/toolbox/v2/tid"
 	"github.com/richardwilkes/toolbox/v2/uti"
 )
 
 // CreatePrivateDataType registers and returns a new data type whose UTI is composed of the "private." prefix, the
 // supplied key, and a random, unique suffix. This is intended for data types that should be unique to this instance of
 // this application, such as those used for drag & drop operations that only make sense within the application. The key
-// should be a dot-separated, reverse-DNS-style string (e.g. "unison.dockable"). The random suffix uses a TID with its
-// underscores replaced by hyphens, since underscores are not valid within a UTI (macOS rejects them) while hyphens are.
+// should be a dot-separated, reverse-DNS-style string (e.g. "unison.dockable").
 func CreatePrivateDataType(key string) *uti.DataType {
-	suffix := strings.ReplaceAll(string(tid.MustNewTID('z')), "_", "-")
-	return uti.Register(&uti.DataType{UTI: "private." + key + "." + suffix})
+	var buffer [12]byte
+	rand.Read(buffer[:])
+	return uti.Register(&uti.DataType{UTI: "private." + key + "." + hex.EncodeToString(buffer[:])})
 }
